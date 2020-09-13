b0453c62848540f1742c40cc3ebfcf9ba6fa69b1
maddytec <maddytec@gmail.com>
2020-09-13 08:32:40 -0300

    Adicionado Template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'sua-financa';
+
+  public opened: boolean = false;
+
+  public toggleSidebar() {
+    this.opened = !this.opened;
+  }
 }
